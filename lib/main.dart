6e37119e29f810379a0cadffafecc37385e57197
Afzal <afzalashraf814@gmail.com>
2023-08-26 03:05:59 +0500

    📦 misc: add starter code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_architecture/user_model.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -11,31 +15,38 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Architecture',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Architecture'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final List<UserModel> users = [];
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  _fetchUsers() async {
+    var url = Uri.parse('https://jsonplaceholder.typicode.com/users');
+    var response = await http.get(url);
+    var list = jsonDecode(response.body) as List;
+    users.addAll(list.map((e) => UserModel.fromJson(e)).toList());
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchUsers();
   }
 
   @override
@@ -44,27 +55,30 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         centerTitle: true,
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text('Flutter Architecture'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+          child: ListView(
+        children: users
+            .map((user) => Container(
+                  margin:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple.shade100,
+                    borderRadius: const BorderRadius.all(Radius.circular(8)),
+                  ),
+                  child: ListTile(
+                    title:
+                        Text(user.name, style: const TextStyle(fontSize: 18)),
+                    subtitle: Text(user.email),
+                    leading: CircleAvatar(
+                      child: Text(user.name[0]),
+                    ),
+                  ),
+                ))
+            .toList(),
+      )),
     );
   }
 }
